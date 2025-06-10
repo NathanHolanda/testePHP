8ea4b96e6bfacce2099714d67a2fd65340f636af
NathanHolanda <nathan.hl.contato@gmail.com>
2025-06-10 17:04:42 -0300

    feat: creating migrations

---
+++ src/database/migrations/2025_06_10_172653_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity');
+            $table->enum('status', ['pending', 'payed', 'cancelled'])->default('pending');
+            $table->dateTime('order_date')->useCurrent();
+            $table->timestamps();
+
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
