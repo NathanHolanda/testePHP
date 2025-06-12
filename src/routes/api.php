3d67af477a6fe5c947d3e5512b9f4daa62042cfd
NathanHolanda <nathan.hl.contato@gmail.com>
2025-06-12 16:12:32 -0300

    feat: creating orders and products endpoints

--- src/routes/api.php
+++ src/routes/api.php
@@ -7,7 +7,33 @@ use App\Http\Controllers\Client\UpdateClientController;
 use App\Http\Controllers\Client\DeleteClientController;
 use App\Http\Controllers\Client\PaginatedClientController;
 
-Route::post('/clients', [CreateClientController::class, 'create'])->name('clients.create');
-Route::put('/clients/{id}', [UpdateClientController::class, 'update'])->name('clients.update');
-Route::delete('/clients/{id}', [DeleteClientController::class, 'delete'])->name('clients.delete');
-Route::get('/clients', [PaginatedClientController::class, 'getPaginatedClients'])->name('clients.paginated');
+use App\Http\Controllers\Product\CreateProductController;
+use App\Http\Controllers\Product\UpdateProductController;
+use App\Http\Controllers\Product\DeleteProductController;
+use App\Http\Controllers\Product\PaginatedProductController;
+
+use App\Http\Controllers\Order\CreateOrderController;
+use App\Http\Controllers\Order\UpdateOrderController;
+use App\Http\Controllers\Order\DeleteOrderController;
+use App\Http\Controllers\Order\PaginatedOrderController;
+
+Route::prefix('clients')->name("clients.")->group(function () {
+    Route::post('/', [CreateClientController::class, 'create'])->name('create');
+    Route::put('/{id}', [UpdateClientController::class, 'update'])->name('update');
+    Route::delete('/{id}', [DeleteClientController::class, 'delete'])->name('delete');
+    Route::get('/', [PaginatedClientController::class, 'getPaginatedClients'])->name('paginated');
+});
+
+Route::prefix('products')->name("products.")->group(function (){
+    Route::post('/', [CreateProductController::class, 'create'])->name('create');
+    Route::put('/{id}', [UpdateProductController::class, 'update'])->name('update');
+    Route::delete('/{id}', [DeleteProductController::class, 'delete'])->name('delete');
+    Route::get('/', [PaginatedProductController::class, 'getPaginatedProducts'])->name('paginated');
+});
+
+Route::prefix('orders')->name("orders.")->group(function (){
+    Route::post('/', [CreateOrderController::class, 'create'])->name('create');
+    Route::put('/{id}', [UpdateOrderController::class, 'update'])->name('update');
+    Route::delete('/{id}', [DeleteOrderController::class, 'delete'])->name('delete');
+    Route::get('/', [PaginatedOrderController::class, 'getPaginatedOrders'])->name('paginated');
+});
